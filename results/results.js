a29d8c8206cf073ab423a3b5efaebd03b1ff68e1
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-21 10:51:25 -0700

    starting to generate html for results page

--- results/results.js
+++ results/results.js
@@ -1,3 +1,25 @@
-import { getPokedex } from '../storage-utils.js';
+import { findById, getPokedex } from '../storage-utils.js';
+import { pokemon } from '../pokemon.js';
 
-const poke = getPokedex();
+const results = getPokedex();
+
+const main = document.getElementById('main');
+
+for (let item of results) {
+    const poke = findById(item.id, pokemon);
+    console.log(item);
+
+    const div = document.createElement('div');
+
+    const img = document.createElement('img');
+    img.src = poke.url_image;
+
+    const header = document.createElement('h2');
+    header.textContent = poke.pokemon;
+
+    const resultSpan1 = document.createElement('span');
+    const resultSpan2 = document.createElement('span');
+
+    div.append(img, header);
+    main.append(div);
+}
