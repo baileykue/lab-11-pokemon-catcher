87eee787237353569a33f5f6f69d36a33e500a69
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-21 10:15:30 -0700

    added ids to inputs in html

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import { pokemon } from './pokemon.js';
-import { findById } from './storage-utils.js';
+import { findById, encounterPoke, capturePoke } from './storage-utils.js';
 
 const poke1Img = document.getElementById('poke-1-img');
 const poke2Img = document.getElementById('poke-2-img');
 const poke3Img = document.getElementById('poke-3-img');
 
 const captureButton = document.getElementById('capture');
+
+const poke1Radio = document.getElementById('poke-1');
 
 const generatePokemon = () =>{
     let randPoke1 = Math.floor(Math.random() * pokemon.length);
@@ -21,22 +23,28 @@ const generatePokemon = () =>{
     }
 
     let poke1 = pokemon[randPoke1];
+    encounterPoke(poke1.id);
     poke1Img.src = poke1.url_image;
+    poke1Radio.value = poke1.id;
+
 
     let poke2 = pokemon[randPoke2];
+    encounterPoke(poke2.id);
     poke2Img.src = poke2.url_image;
 
     let poke3 = pokemon[randPoke3];
+    encounterPoke(poke3.id);
     poke3Img.src = poke3.url_image;
-    console.log(poke1, poke2, poke3);
 };
+
+let totalPlays = 0;
 
 generatePokemon();
 
 captureButton.addEventListener('click', ()=>{
-    //capturePokemon();
+    totalPlays++;
+
     generatePokemon();
-    // const userChoice = document.querySelector('input[type=radio]:checked');
-    // console.log(findById(userChoice, pokemon));
+
 
 });
