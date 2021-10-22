cf51ee3eb6deb6899f966067093fe4fa6f4fc426
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-21 17:35:25 -0700

    started adding the chart & data

--- results/results.js
+++ results/results.js
@@ -26,3 +26,47 @@ for (let item of results) {
     main.append(div);
 }
 
+const names = results.map((item)=>{
+    const poke = findById(item.id, pokemon);
+    return poke.pokemon;
+});
+
+const captured = results.map(item=>item.captured);
+
+const ctx = document.getElementById('resultsChart').getContext('2d');
+// eslint-disable-next-line no-undef
+new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: names,
+        datasets: [{
+            label: '# of Times Captured',
+            data: captured, 
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+        
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
