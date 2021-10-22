64fafe2f7525336c7cafe7be820f8568d81504d8
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-22 08:27:34 -0700

    messing around trying to animate the chart

--- results/results.js
+++ results/results.js
@@ -43,26 +43,29 @@ new Chart(ctx, {
             label: '# of Times Captured',
             data: captured, 
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 206, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(153, 102, 255)',
+                'rgb(255, 159, 64)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 206, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(153, 102, 255)',
+                'rgb(255, 159, 64)'
             ],
             borderWidth: 1
         }]
         
     },
     options: {
+        animations: {
+            animateScale: true
+        },
         scales: {
             y: {
                 beginAtZero: true
